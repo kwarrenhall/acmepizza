dea5a91a7f9f4f17399c3d9a141423a58c594bcc
Kate Hall <kwarrenhall@gmail.com>
2016-01-29 09:20:15 -0800

    first failing spec test for price method

--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,11 @@
 describe ('pizza', function() {
   it("creates a new pizza order with the given properties", function() {
-    var testPizza = new Pizza(1, "large");
-    expect(testPizza.topping).to.equal(1);
-    expect(testPizza.size).to.equal("large");
+  var testPizza = new Pizza(1, "large");
+  expect(testPizza.topping).to.equal(1);
+  expect(testPizza.size).to.equal("large");
+  });
+  it("adds the price method to the pizza constructor", function() {
+  var testPizza = new Pizza(1, "large");
+  expect(testPizza.price()).to.equal(10);
   });
 });
