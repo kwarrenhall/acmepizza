442abd0a725d1d40838daff65d3e929eb00a0776
Kate Hall <kwarrenhall@gmail.com>
2016-01-29 09:15:31 -0800

    first passing scripts test for pizza  constructor

--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,7 @@
 describe ('pizza', function() {
   it("creates a new pizza order with the given properties", function() {
-    var testPizza = new Pizza("large", 1);
-    expect(testPizza.toppings).to.equal(1);
+    var testPizza = new Pizza(1, "large");
+    expect(testPizza.topping).to.equal(1);
     expect(testPizza.size).to.equal("large");
   });
 });
