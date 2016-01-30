1407783383a400f8d207b9b43d030c14ffb5da68
Kate Hall <kwarrenhall@gmail.com>
2016-01-29 16:40:54 -0800

    add front end with working size and topping.  Price not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,58 @@
-function Pizza(topping, size) {
-  this.topping = topping;
+function Pizza(size, topping) {
   this.size = size;
+  this.topping = topping;
+
 }
 
 Pizza.prototype.price = function() {
     var price = 10;
-    var size = this.size
+    var sizes = this.size
     var topping = this.topping
 
-    if ((size === "large") && (topping === 1)) {
-      return price +=1;
-  } else if ((size === "large") && (topping === 2)) {
-    return price +=2;
-  } else if ((size === "large") && (topping === 2)) {
-    return price +=3;
-  } else if ((size === "medium") && (topping === 1)) {
+    if ((sizes === "large") && (topping === 1)) {
+      return price += 1;
+  } else if ((sizes === "large") && (topping === 2)) {
+    return price += 2;
+  } else if ((sizes === "large") && (topping === 2)) {
+    return price += 3;
+  } else if ((sizes === "medium") && (topping === 1)) {
     return price;
-  } else if ((size === "medium") && (topping === 2)) {
-    return price +=1;
-  } else if ((size === "medium") && (topping === 3)) {
-    return price +=2;
-  } else if ((size === "small") && (topping === 1)) {
-    return price -=1;
-  } else if ((size === "small") && (topping === 2)) {
+  } else if ((sizes === "medium") && (topping === 2)) {
+    return price += 1;
+  } else if ((sizes === "medium") && (topping === 3)) {
+    return price += 2;
+  } else if ((sizes === "small") && (topping === 1)) {
+    return price -= 1;
+  } else if ((sizes === "small") && (topping === 2)) {
     return price;
-  } else if ((size === "small") && (topping === 3)) {
-    return price +=1;
-  } else {
-    return price;
+  } else if ((sizes === "small") && (topping === 3)) {
+    return price += 1;
   }
+    return price;
   };
 
-Interface//
-  $(document).ready(function() {
+// Interface//
+$(document).ready(function() {
   var newPizza;
   $("form#pizza").submit(function(event) {
       event.preventDefault();
 
-      var pizzaSize= $("radio#size").val();
-      var pizzaTopping= $("radio#topping").val();
+      var pizzaSize = $("select#size").val();
+      var pizzaTopping = $("select#topping").val();
       var newPizza = new Pizza(pizzaSize, pizzaTopping);
       var price = newPizza.price();
 
- $(".show-pizza").show();
- $("#pizzaPrice").text(" $" + price);
+    if (pizzaSize === "" || pizzaTopping === "") {
+      return alert("Please make all selections");
+      $(".show-price").hide();
+    }
 
 
- });
+  $(".show-price").show();
+  $("#showPizzaSize").text(" " + pizzaSize);
+  $("#pizzaTopping").text("" + pizzaTopping);
+  $("#pizzaPrice").text(" $" + price);
+
+
+  });
 });
