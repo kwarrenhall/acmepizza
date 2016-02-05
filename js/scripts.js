bd47fc3229debcd3e48f7f96dda064b066e4c672
Kate Hall <kwarrenhall@gmail.com>
2016-02-04 18:31:01 -0800

    add working tests and front end fortwo toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,81 @@
-function Pizza(size, topping) {
+function Pizza(size, topping1, topping2) {
   this.size = size;
-  this.topping = topping;
-
+  this.topping1 = topping1;
+  this.topping2 = topping2;
 }
 
 Pizza.prototype.price = function() {
     var price = 10;
 
-    if ((this.size === "Large") && (this.topping === "Pepperoni")) {
-      return price += 1;
-  } else if ((this.size === "Large") && (this.topping === "Jalapeno")) {
+  if ((this.size === "Large") && (this.topping1 === "Pepperoni" && this.topping2 === "Mushroom")) {
+      return price += 3;
+  } else if ((this.size === "Large") && (this.topping1 === "Pepperoni" && this.topping2 === "Jalapeno")) {
+    return price += 3;
+  } else if ((this.size === "Large") && (this.topping1 === "Pepperoni" && this.topping === "Garlic")) {
+    return price += 3;
+  } else if ((this.size === "Large") && (this.topping1 === "Sausage" && this.topping2 === "Mushroom")) {
+    return price += 3;
+  } else if ((this.size === "Large") && (this.topping1 === "Sausage" && this.topping2 === "Jalapeno")) {
+    return price += 3;
+  } else if ((this.size === "Large") && (this.topping1 === "Sausage" && this.topping2 === "Garlic")) {
+    return price += 3;
+  } else if ((this.size === "Large") && (this.topping1 === "Peperoni")) {
+    return price += 2;
+  } else if ((this.size === "Large") && (this.topping1 === "Sausage")) {
+    return price += 2;
+  } else if ((this.size === "Large") && (this.topping2 === "Mushroom")) {
+    return price += 2;
+  } else if ((this.size === "Large") && (this.topping2 === "Jalapeno")) {
+    return price += 2;
+  } else if ((this.size === "Large") && (this.topping2 === "Garlic")) {
+    return price += 2;
+  } else if ((this.size === "Medium") && (this.topping1 === "Pepperoni" && this.topping2 === "Mushroom")) {
     return price += 1;
-  } else if ((this.size === "Large") && (this.topping === "Garlic")) {
+  } else if ((this.size === "Medium") && (this.topping1 === "Pepperoni" && this.topping2 === "Jalapeno")) {
     return price += 1;
-  } else if ((this.size === "Medium") && (this.topping === "Pepperoni")) {
+  } else if ((this.size === "Medium") && (this.topping1 === "Pepperoni" && this.topping === "Garlic")) {
+    return price += 1;
+  } else if ((this.size === "Medium") && (this.topping1 === "Sausage" && this.topping2 === "Mushroom")) {
+    return price += 1;
+  } else if ((this.size === "Medium") && (this.topping1 === "Sausage" && this.topping2 === "Jalapeno")) {
+    return price += 1;
+  } else if ((this.size === "Medium") && (this.topping1 === "Sausage" && this.topping2 === "Garlic")) {
+    return price += 1;
+  } else if ((this.size === "Medium") && (this.topping1 === "Peperoni")) {
     return price;
-  } else if ((this.size === "Medium") && (this.topping === "Jalapeno")) {
+  } else if ((this.size === "Medium") && (this.topping1 === "Sausage")) {
     return price;
-  } else if ((this.size === "Medium") && (this.topping === "Garlic")) {
+  } else if ((this.size === "Medium") && (this.topping2 === "Mushroom")) {
     return price;
-  } else if ((this.size === "Small") && (this.topping === "Pepperoni")) {
+  } else if ((this.size === "Medium") && (this.topping2 === "Jalapeno")) {
+    return price;
+  } else if ((this.size === "Medium") && (this.topping2 === "Garlic")) {
+    return price;
+  } else if ((this.size === "Small") && (this.topping1 === "Pepperoni" && this.topping2 === "Mushroom")) {
+      return price -= 1;
+  } else if ((this.size === "Small") && (this.topping1 === "Pepperoni" && this.topping2 === "Jalapeno")) {
     return price -= 1;
-  } else if ((this.size === "Small") && (this.topping === "Jalapeno")) {
+  } else if ((this.size === "Small") && (this.topping1 === "Pepperoni" && this.topping2 === "Garlic")) {
     return price -= 1;
-  } else if ((this.size === "Small") && (this.topping === "Garlic")) {
+  } else if ((this.size === "Small") && (this.topping1 === "Sausage" && this.topping2 === "Mushroom")) {
     return price -= 1;
+  } else if ((this.size === "Small") && (this.topping1 === "Sausage" && this.topping2 === "Jalapeno")) {
+    return price -= 1;
+  } else if ((this.size === "Small") && (this.topping1 === "Sausage" && this.topping === "Garlic")) {
+    return price -= 1;
+  } else if ((this.size === "Small") && (this.topping1 === "Peperoni")) {
+    return price -= 2;
+  } else if ((this.size === "Small") && (this.topping1 === "Sausage")) {
+    return price -= 2;
+  } else if ((this.size === "Small") && (this.topping2 === "Mushroom")) {
+    return price -= 2;
+  } else if ((this.size === "Small") && (this.topping2 === "Jalapeno")) {
+    return price -= 2;
+  } else if ((this.size === "Small") && (this.topping2 === "Garlic")) {
+    return price -= 2;
   }
     return price;
-  };
+  }
 
 // Interface//
 $(document).ready(function() {
@@ -36,19 +84,21 @@ $(document).ready(function() {
       event.preventDefault();
 
       var pizzaSize = $("select#size").val();
-      var pizzaTopping = $("select#topping").val();
-      var newPizza = new Pizza(pizzaSize, pizzaTopping);
+      var pizzaTopping1 = $("select#topping1").val();
+      var pizzaTopping2 = $("select#topping2").val();
+      var newPizza = new Pizza(pizzaSize, pizzaTopping1, pizzaTopping2);
       var price = newPizza.price();
 
-    if (pizzaSize === "" || pizzaTopping === "") {
-      return alert("Please make all selections");
-      $(".show-price").hide();
-    }
-
+    // if (pizzaSize === "" || pizzaTopping === "") {
+    //   return alert("Please make all selections");
+    //   $(".show-price").hide();
+    // }
+    
 
   $(".show-price").show();
   $("#showPizzaSize").text(" " + pizzaSize);
-  $("#pizzaTopping").text("" + pizzaTopping);
+  $("#pizzaTopping1").text("" + pizzaTopping1);
+  $("#pizzaTopping2").text("" + pizzaTopping2);
   $("#pizzaPrice").text(" $" + price);
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,12 @@
 describe ('pizza', function() {
   it("creates a new pizza order with the given properties", function() {
-  var testPizza = new Pizza("Large", "Pepperoni");
+  var testPizza = new Pizza("Large", "Pepperoni", "Mushroom");
   expect(testPizza.size).to.equal("Large");
-  expect(testPizza.topping).to.equal("Pepperoni");
+  expect(testPizza.topping1).to.equal("Pepperoni");
+  expect(testPizza.topping2).to.equal("Mushroom");
   });
   it("adds the price method to the pizza constructor", function() {
-  var testPizza = new Pizza("Large", "Pepperoni");
-  expect(testPizza.price()).to.equal(11);
+  var testPizza = new Pizza("Large", "Pepperoni", "Mushroom");
+  expect(testPizza.price()).to.equal(13);
   });
 });
